6e2423a17e622b5fff4511469e543274558fc51b
Huy Chau <phimmuot@gmail.com>
2024-09-14 22:28:20 +0700

    Update settings.py

--- backend/webkientruc/settings.py
+++ backend/webkientruc/settings.py
@@ -159,7 +159,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://ota.phapsuit.com/'
+    'https://ota.phapsuit.com',
 ]
 
 CORS_ORIGINS_ALLOW_ALL = True
